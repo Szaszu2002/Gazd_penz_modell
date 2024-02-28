2ab9799eef41df4d697729ad2f688c4218d7707a
Szabó Alexandra Marianna <90461871+Szaszu2002@users.noreply.github.com>
2024-02-28 11:29:55 +0100

    gyak3

---
+++ nem_linreg.R
@@ -0,0 +1,29 @@
+nem_linreg=function(){
+x=c(1,2,3,4)
+n=length(x)
+
+y[1]=2.7182+0.05
+y[2]=7.3890-0.13
+y[3]=20.0855+0.15
+y[4]=54.5981-0.18
+y=log(y)
+plot(x,y)
+
+a1=0
+b1=0
+c1=0
+a2=0
+c2=0
+for (i in 1:n){
+	a1=a1+x[i]*y[i]
+	b1=b1+(-x[i])
+	c1=c1+y[i]
+
+	a2=a2+x[i]^2
+	c2=c2+x[i]
+}
+a=(a1*n-c1*b1)/(a2*n-c2*b1)
+b=(a2*c1-c2*a1)/(a2*n-c2*b1)
+print(a)
+print(b)
+}
